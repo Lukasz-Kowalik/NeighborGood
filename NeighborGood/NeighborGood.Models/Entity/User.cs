5345425514187a4ccaf697405ec4a481a29a50d3
DawidSieczka <dawid.sieczka@pirum-technology.com>
2020-11-20 20:58:05 +0100

    added projects

---
+++ NeighborGood/NeighborGood.Models/Entity/User.cs
@@ -0,0 +1,16 @@
+﻿using NeighborGood.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeighborGood.Models.Entity
+{
+    public class User : BaseModel
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public decimal Rate { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
