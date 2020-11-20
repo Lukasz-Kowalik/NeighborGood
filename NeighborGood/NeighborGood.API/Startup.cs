0750a4a074ade1a8e2297d9c8d91abad2e9f2c82
Lukasz-Kowalik <lukasz.kowalik41@gmail.com>
2020-11-20 21:22:42 +0100

    added AccountController

--- NeighborGood/NeighborGood.API/Startup.cs
+++ NeighborGood/NeighborGood.API/Startup.cs
@@ -1,3 +1,4 @@
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,7 @@ namespace NeighborGood.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddMediatR(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
